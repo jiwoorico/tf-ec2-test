0fe738816ab6f2330c0d571a2b5bd3fa577709a1
Rico Shen <binbin.shen@autodesk.com>
2023-06-12 15:40:29 +0800

    1

---
+++ main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+}
+
+
+module "ec2_instance" {
+  source  = "git::git@github.com:jiwoorico/tf-modules-ec2.git//?ref=v4.4.0"
+
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
